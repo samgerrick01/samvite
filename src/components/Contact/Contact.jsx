018bade3f76352624f2e1a536df1c21a633af2f2
SamGerrick <sgcdesilva@unionbankph.com>
2023-05-23 22:56:55 +0800

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,78 @@
+import React, { useContext, useRef, useState } from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import { themeContext } from "../../Context";
+const Contact = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  const form = useRef();
+  const [done, setDone] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qiw5983",
+        "template_y4a841u",
+        form.current,
+        "user_DDFsRffPS1lZsdHbfvwDT"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact-form" id="contact">
+      {/* left side copy and paste from work section */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* darkMode */}
+          <span style={{ color: darkMode ? "white" : "" }}>Get in Touch</span>
+          <span>Contact me</span>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+      </div>
+      {/* right side form */}
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_Name"
+            className="user"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="user_Email"
+            className="user"
+            placeholder="Email"
+          />
+          <textarea
+            name="user_Message"
+            className="user"
+            placeholder="Message"
+          />
+          <input type="submit" value="Send" className="button" />
+          <span>{done && "Thanks for Contacting me"}</span>
+          <div
+            className="blur c-blur1"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Toggle from "../Toggle/Toggle";
+import ReactJS from "../../img/reactjs.png";
+import JavaScript from "../../img/js.png";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+const navbar = () => {
+  return (
+    <div className="n-wrapper" id="Navbar">
+      {/* left */}
+      <div className="n-left">
+        <img src={ReactJS} width={72} height={52} alt="" />
+        <img src={JavaScript} width={72} height={42} alt="" />
+        <Toggle />
+      </div>
+      {/* right */}
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                Serivces
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy={true} smooth={true}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy={true} smooth={true}>
+                Protfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy={true} smooth={true}>
+                Testimonial
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="contact" spy={true} smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
