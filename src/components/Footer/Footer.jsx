a5c181daf23a6485113ca8951f0b9411cc64f27c
SamGerrick <sgcdesilva@unionbankph.com>
2023-05-24 08:48:14 +0800

    2nd commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
-// import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-// import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-// import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>Zainkeepscode@gmail.com</span>
+        <span>desilva.sam17.sgds@gmail.com</span>
         <div className="f-icons">
-          {/* <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} /> */}
+          <FaInstagram color="white" size={"3rem"} />
+          <FaFacebook color="white" size={"3rem"} />
+          <FaGithub color="white" size={"3rem"} />
         </div>
       </div>
     </div>
